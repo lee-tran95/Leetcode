20d4a309766ee1e02907964f8ab9af51b7d19e39
Lee Tran <lee.tran95@yahoo.com>
2022-06-25 18:37:42 -0700

    Reversea linked list

---
+++ linked_list_01.js
@@ -0,0 +1,31 @@
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+// Example 2:
+
+
+// Input: head = [1,2]
+// Output: [2,1]
+// Example 3:
+
+// Input: head = []
+// Output: []
+
+var reverseList = function(head) {
+    let prev = null;
+    let current = head;
+    let next;
+    while(current){
+        next = current.next
+        current.next = prev
+        prev = current;
+        current = next
+    }
+    return prev
+};
