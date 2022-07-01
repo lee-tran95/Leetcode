bd1187421b978a8a32a58ef3fb5da077692509b6
Lee Tran <lee.tran95@yahoo.com>
2022-07-01 01:10:28 -0700

    Use floyd's algo in order to find the duplicate number in an array

---
+++ linked_list_07.js
@@ -0,0 +1,45 @@
+// 287. Find the Duplicate Number
+// Medium
+
+// 14713
+
+// 1821
+
+// Add to List
+
+// Share
+// Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+// There is only one repeated number in nums, return this repeated number.
+
+// You must solve the problem without modifying the array nums and uses only constant extra space.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,3,4,2,2]
+// Output: 2
+// Example 2:
+
+// Input: nums = [3,1,3,4,2]
+// Output: 3
+
+var findDuplicate = function(nums) {
+    let slow = 0;
+    let fast = 0;
+    while(true){
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        if(slow === fast) break;
+    }
+    
+    let slow2 = 0
+    while(true){
+        slow = nums[slow]
+        slow2 = nums[slow2]
+        if(slow === slow2) break;
+    }
+    
+    return slow
+};
