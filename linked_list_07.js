07eaeea1615d4d8745dec533c9fb21c5787893c9
Lee Tran <lee.tran95@yahoo.com>
2022-08-20 23:07:04 -0700

    Re-solve find the duplicate number in the array without using extra space

--- linked_list_07.js
+++ linked_list_07.js
@@ -43,3 +43,28 @@ var findDuplicate = function(nums) {
     
     return slow
 };
+
+var findDuplicate = function(nums) {
+    let slow = 0;
+    let fast = 0;
+    
+    while(true){
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        if(nums[slow] === nums[fast]){
+            break
+        }
+    }
+
+    let slow2 = 0;
+    while(true){
+        slow = nums[slow]
+        slow2 = nums[slow2]
+        if(slow === slow2){
+            return slow
+        }
+    }
+};
+
+// O(n) time complexity
+// O(1) space complexity
