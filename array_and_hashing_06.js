734bc2bf6920f80f166b6b4feae062efde6e9e33
Lee Tran <lee.tran95@yahoo.com>
2022-08-28 19:04:26 -0700

    Refactored the code so it no longers create an array of ones

--- array_and_hashing_06.js
+++ array_and_hashing_06.js
@@ -31,3 +31,22 @@ var productExceptSelf = function(nums) {
     }
     return res
 };
+
+var productExceptSelf = function(nums) {
+    let res = [];
+    
+    let left = 1;
+    for(let i = 0; i < nums.length;i++){
+        res[i] = left
+        left *= nums[i]
+    }
+    let right = 1
+    for(let i = nums.length -1; i >= 0; i--){
+        res[i] *= right
+        right *= nums[i]
+    }
+    return res
+};
+
+// O(n) Time complexity
+// O(1) space
