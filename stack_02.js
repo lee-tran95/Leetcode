28ae6023a4e67f2c8b14ba7e95375e0e3581da5f
Lee Tran <lee.tran95@yahoo.com>
2022-09-08 14:14:14 -0700

    Re-solve the min stack problem and added time complexity

--- stack_02.js
+++ stack_02.js
@@ -77,3 +77,48 @@ MinStack.prototype.getMin = function() {
  * var param_3 = obj.top()
  * var param_4 = obj.getMin()
  */
+
+
+
+ var MinStack = function() {
+    this.stack = [];
+    this.minStack = [];
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    this.stack[this.stack.length] = val
+    if(this.minStack.length > 0){
+        val = Math.min(val, this.minStack[this.minStack.length-1])
+        this.minStack[this.minStack.length] = val
+    }else{
+        this.minStack[this.minStack.length] = val
+    }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    this.stack.pop()
+    this.minStack.pop()
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length-1]
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.minStack[this.minStack.length-1]
+};
+
+//O(1) time complexity
