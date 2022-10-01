570b495065eca6750c32aad55e9c91e2f852c395
Lee Tran <lee.tran95@yahoo.com>
2022-10-01 01:04:22 -0700

    re-solve delete nth node from the end of a linked list and add time and space complexity

--- linked_list_04.js
+++ linked_list_04.js
@@ -33,3 +33,22 @@ var removeNthFromEnd = function(head, n) {
     prev.next = prev.next.next
     return dummy.next
 };
+
+var removeNthFromEnd = function(head, n) {
+    let dummy = new ListNode()
+    dummy.next = head;
+   let fast = dummy;
+   let slow = dummy;
+   for(let i = 0; i < n; i++){
+       fast = fast.next
+   }
+   while(fast.next){
+       fast = fast.next;
+       slow = slow.next;
+   }
+   slow.next = slow.next.next
+   return dummy.next
+};
+
+// O(n) time complexity
+// O(1) space complexity
