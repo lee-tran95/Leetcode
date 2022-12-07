22b050e2dab52e38cdd3ddf85031b9143e21f6cc
Lee Tran <lee.tran95@yahoo.com>
2022-12-07 01:13:46 -0800

    Combination sum IV using memoization

---
+++ dynamic_programming_07.js
@@ -0,0 +1,42 @@
+// Given an array of distinct integers nums and a target integer target, return the number of possible combinations that add up to target.
+
+// The test cases are generated so that the answer can fit in a 32-bit integer.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3], target = 4
+// Output: 7
+// Explanation:
+// The possible combination ways are:
+// (1, 1, 1, 1)
+// (1, 1, 2)
+// (1, 2, 1)
+// (1, 3)
+// (2, 1, 1)
+// (2, 2)
+// (3, 1)
+// Note that different sequences are counted as different combinations.
+// Example 2:
+
+// Input: nums = [9], target = 3
+// Output: 0
+
+var combinationSum4 = function(nums, target) {
+    let memo = new Map()
+
+    function dfs(target){
+        if(memo.has(target)) return memo.get(target)
+        if(target === 0) return 1
+        if(target < 0) return 0
+
+        let count = 0
+        for(let num of nums){
+            count += dfs(target-num)
+        }
+        memo.set(target,count)
+        return count
+    }
+    return dfs(target)
+};
