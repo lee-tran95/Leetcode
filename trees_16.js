b2b27ce29aac62329e54ad27c7b782762e69134c
Lee Tran <lee.tran95@yahoo.com>
2022-12-19 22:38:35 -0800

    Convert a given string into a binary tree

---
+++ trees_16.js
@@ -0,0 +1,40 @@
+// Convert a given string into a tree
+
+class TreeNode{
+    constructor(val, left = null, right =null){
+      this.val = val;
+      this.left = left;
+      this.right = right
+    }
+  }
+  
+  function strTwoTree(string){
+    function helper(substring){
+      if(!substring || substring.length === 0){
+        return null
+      }
+      let leftParen = substring.indexOf('(')
+      let rightParen = 0;
+      if(leftParen === -1) return Number(substring)
+      let root = new TreeNode(Number(substring.slice(0,leftParen)))
+      let count = 0
+  
+      for(let i = leftParen; i < substring.length; i++){
+        if(substring[i] === '('){
+          count += 1
+        }else if(substring[i] === ')'){
+          count -= 1
+        }
+        if(count === 0){
+          rightParen = i
+          break
+        }
+      }
+      root.left = helper(substring.slice(leftParen+1, rightParen))
+      root.right = helper(substring.slice(rightParen + 2, substring.length -1))
+      return root
+    }
+    return helper(string)
+  }
+  
+  console.log(strTwoTree('4(2(3)(1))(6(5))'))
